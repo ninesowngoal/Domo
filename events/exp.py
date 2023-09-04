78d7c28cc3ce65be92aabfa7ff7b8499ad271c34
ninesowngoal <c.chipeta1@gmail.com>
2023-09-04 17:58:17 +0100

    Updated exp tracker

--- events/exp.py
+++ events/exp.py
@@ -17,9 +17,9 @@ class MemberExp:
 
     # - Calculate level up.
     def level_up(self):
-        self.curr_xp -= self.xp_to_next
-        self.xp_to_next = 1000
-        self.level += 1
+        new_level = self.curr_xp // self.xp_to_next
+        self.curr_xp %= self.xp_to_next # - removes remainders from above equation
+        self.level = new_level
 
 class MemberExpDatabase:
     # - SQL database for member, exp and level.
@@ -45,14 +45,14 @@ class MemberExpDatabase:
                                    (member_id, level, curr_xp))
         self.db_connection.commit()
 
-# - Function to load data from the database into the dictionary on bot startup
+# Function to load data from the database into the dictionary on bot startup
 def load_data_from_database():
     absolute_path = os.path.dirname(__file__)
     db_file = os.path.join(absolute_path, "experience.db")
     db_con = sqlite3.connect(db_file)
     cur = db_con.cursor()
 
-    # - Fetch data from the database and update the member_exp_dict
+    # Fetch data from the database and update the member_exp_dict
     cur.execute("SELECT member, level, xp FROM experience")
     data = cur.fetchall()
 
@@ -88,7 +88,6 @@ def member_exp(bot):
             return
 
         member_id = message.author.id
-        channel = bot.get_channel(YOUR_CHANNEL_ID_HERE) # - Can delete if you want.
         ctx = await bot.get_context(message)
 
         # - Gets or creates a MemberExp instance for the member
@@ -106,6 +105,49 @@ def member_exp(bot):
 
         # - Check if the member has leveled up
         if member_exp.level > previous_level:
-            await channel.send(f"{message.author} has levelled up to level {member_exp.level}!")
-            # - Delete above line if you deleted the channel variable.
             await ctx.send(f"<@{message.author.id}> has levelled up to level {member_exp.level}!")
+    
+    import time
+
+    @bot.event
+    async def on_voice_state_update(member, before, after):
+        # - Checks when a user has joined the voice channel
+        # - and awards xp based on how long they have been
+        # - in the voice channel for.
+        member_id = member.id
+        channel = YOUR_TEXT_CHANNEL_ID_HERE
+
+        member_exp = member_exp_dict.get(member_id)
+        if member_exp is None:
+            member_exp = MemberExp(member_id)
+            member_exp_dict[member_id] = member_exp
+    
+        xp_per_minute = 16.67
+        previous_level = member_exp.level
+        elapsed_time = time.time()
+
+        if before.channel is None and after.channel is not None:
+            if member.deafen:
+                # - Doesn't record if member is deafened.
+                return
+            elif before.channel is None and after.channel == YOUR_AFK_CHANNEL_ID_HERE:
+                # - Won't track if in afk.
+                return
+            else:
+                elapsed_time = time.time()  # - Records the time when the user joins
+
+        elif before.channel is not None and after.channel is None:
+            if member.deafen:
+            # - Doesn't record if member is deafened.
+                return
+            elapsed_time = time.time() - elapsed_time  # - Calculates the time spent in the channel
+
+    # - Continue tracking when undeafened (assuming not muted and deafened)
+        if not member.deafen:
+        # - Calculate XP based on elapsed time
+            xp_earned = int(elapsed_time / 60) * xp_per_minute  # - XP per minute
+            member_exp.gain_xp(xp_earned)
+            member_exp_db.save_member_exp(member_id, member_exp.level, member_exp.curr_xp)
+
+        if member_exp.level > previous_level:
+            await channel.send(f"<@{member_id}> has levelled up to level {member_exp.level}!")
